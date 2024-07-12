02913c754a4e7f1743d7b506f833be356e9bfcf5
Antonio <antonio.smsobrinho@gmail.com>
2024-07-12 16:33:22 -0300

    client app

---
+++ apps/client/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from apps.commons.models import Bank, BaseModel
+
+
+class InvestorProfile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField()
+    address = models.TextField()
+    amount = models.DecimalField(default=0, decimal_places=2, max_digits=500)
+
+
+class InvestorBankAccount(BaseModel):
+    class Meta:
+        unique_together = [["bank", "account_number"]]
+
+    investor = models.ForeignKey(InvestorProfile, on_delete=models.CASCADE)
+    bank = models.ForeignKey(Bank, on_delete=models.PROTECT)
+    account_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.investor.user.username + self.bank.name
+
+
+class BankTransition(BaseModel):
+    amount = models.DecimalField(decimal_places=2, max_digits=500)
+    bank_account = models.ForeignKey(InvestorBankAccount, on_delete=models.PROTECT)
--- apps/commons/models.py
+++ apps/commons/models.py
@@ -8,6 +8,8 @@ class BaseModel(models.Model):
         abstract = True
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Bank(BaseModel):
