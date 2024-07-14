b22ed3e452162d188b90e8eead48c25c7b565632
Antonio <antonio.smsobrinho@gmail.com>
2024-07-14 14:21:43 -0300

    swagger

--- apps/investments/api/viewsets.py
+++ apps/investments/api/viewsets.py
@@ -1,14 +1,19 @@
+from posixpath import abspath
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from apps.clients.api.permissions import HasInvestorProfile
 from apps.investments.services import CurrencyService
+from broker.swagger import extend_schema_from_yaml
 
 
 class CurrenciesView(APIView):
     permission_classes = [IsAuthenticated, HasInvestorProfile]
 
+    @extend_schema_from_yaml(
+        abspath("apps/investments/swagger/currencies/get.yml"),
+    )
     def get(self, request, *args, **kwargs):
         try:
             currencies = CurrencyService.get_currency_values()
